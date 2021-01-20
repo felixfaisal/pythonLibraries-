f314b2545321a37f70104706fa6624660770e2b1
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-01-20 22:25:37 +0530

    Converted into modular code

--- index.py
+++ index.py
@@ -4,30 +4,32 @@ import subprocess
 #import logger
 
 # subprocess is better alternative of OS commands
-filepath = 'HelloWorld.c'
-proc = subprocess.Popen(['gcc', filepath],
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-proc = subprocess.Popen(['./a.out'],
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, stderr = proc.communicate()
-#logger.info('Ran ngSpice command')
-#print("Ran NGSpice command")
-print(stdout)
-filepath = 'HelloWorld.cpp'
-proc = subprocess.Popen(['g++', filepath],
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-proc = subprocess.Popen(['./a.out'],
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, stderr = proc.communicate()
-#logger.info('Ran ngSpice command')
-#print("Ran NGSpice command")
-print(stdout)
-filepath = 'HelloWorld.js'
-proc = subprocess.Popen(['node', filepath],
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-# proc = subprocess.Popen(['./a.out'],
-# stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, stderr = proc.communicate()
-#logger.info('Ran ngSpice command')
-#print("Ran NGSpice command")
-print(stdout)
+
+
+def C_obtainOutput(filepath):
+    proc = subprocess.Popen(['gcc', filepath],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen(['./a.out'],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    return stdout
+
+
+def Cpp_obtainOutput(filepath):
+    proc = subprocess.Popen(['g++', filepath],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen(['./a.out'],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    # print(stdout)
+    return stdout
+
+
+def js_obtainOutput(filepath):
+    proc = subprocess.Popen(['node', filepath],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    print(stdout)
+
+
+print(Cpp_obtainOutput('HelloWorld.cpp').decode())
