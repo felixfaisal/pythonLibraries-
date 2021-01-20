dabd05c8b1a8e28b358fc2311eab55fc3118fbf4
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-01-20 19:21:59 +0530

    Created testing for C,JS and C++

--- index.py
+++ index.py
@@ -1,2 +1,33 @@
-import os 
-os.system("ngspice --v")
+import os
+import logging
+import subprocess
+#import logger
+
+# subprocess is better alternative of OS commands
+filepath = 'HelloWorld.c'
+proc = subprocess.Popen(['gcc', filepath],
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+proc = subprocess.Popen(['./a.out'],
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout, stderr = proc.communicate()
+#logger.info('Ran ngSpice command')
+#print("Ran NGSpice command")
+print(stdout)
+filepath = 'HelloWorld.cpp'
+proc = subprocess.Popen(['g++', filepath],
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+proc = subprocess.Popen(['./a.out'],
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout, stderr = proc.communicate()
+#logger.info('Ran ngSpice command')
+#print("Ran NGSpice command")
+print(stdout)
+filepath = 'HelloWorld.js'
+proc = subprocess.Popen(['node', filepath],
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# proc = subprocess.Popen(['./a.out'],
+# stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout, stderr = proc.communicate()
+#logger.info('Ran ngSpice command')
+#print("Ran NGSpice command")
+print(stdout)
